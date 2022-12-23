f3a3cd00d633c8d1762902b94fbed5519241d7c2
Sergey <aseev-1977@list.ru>
2022-12-23 23:00:43 +0300

    Задача 10

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+int num2 = (num1 % 100) / 10;
+
+Console.WriteLine("\nВ числе "
++ num1 + "\nВторое число -> " + num2 + "\n\n");
